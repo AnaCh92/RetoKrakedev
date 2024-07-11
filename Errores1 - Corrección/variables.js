9c5797764f17e8aabfca88eac40c430e96bc2850
Ana <33724863+AnaCh92@users.noreply.github.com>
2024-07-10 22:26:31 -0500

    Reto Krakedev
    Reto

---
+++ Errores1 - Corrección/variables.js
@@ -0,0 +1,27 @@
+//Variable puntaje
+let puntaje =0;
+
+
+cambiar = function(){
+    puntaje = 1000;
+}
+
+mostrar = function(){
+    alert("El puntaje es " + puntaje);
+}
+
+incrementarUno = function(){
+    puntaje += 1;
+}
+
+decrementarUno = function(){
+    puntaje -=  1;
+}
+
+incrementarDiez = function(){
+    puntaje += 10;
+}
+
+decrementarDiez = function(){
+    puntaje -= 10;
+}
